32b357bbf42d82fc2c077669229b81c77182acc8
vamshe531 <t.vamshi@outlook.com>
2018-02-10 23:11:45 -0600

    Updated with new code

--- Project2/web_app/server.py
+++ Project2/web_app/server.py
@@ -17,21 +17,17 @@ def index():
     return render_template("index.html",
                            title='Image Classification Demo')
 
-
-@app.route('/download', methods=['POST'])
-def download_image():
-    address = request.form['address']
-    modelname = request.form['modelname']
-    filename = os.path.join(UPLOAD, address.split('/')[-1])
-    return predict(filename)
-
-
-@app.route('/upload', methods=['POST'])
-def upload_image():
-    f = request.files['image']
-    filename = os.path.join(UPLOAD, f.filename)
-    f.save(filename)
-    return predict(filename)
+@app.route('/predict', methods=['POST'])
+def predict():
+    modelname = request.form['model']
+    if request.form['imagepath'] == "Submit":
+        address = request.form['address']
+        filename = os.path.join(UPLOAD, address.split('/')[-1])
+    else:
+        f = request.files['image']
+        filename = os.path.join(UPLOAD, f.filename)
+        f.save(filename)
+    return process(filename)
 
 
 @app.route('/' + UPLOAD + '/<path:path>')
@@ -39,7 +35,7 @@ def serve_files(path):
     return send_from_directory(UPLOAD, path)
 
 
-def predict(filename):
+def process(filename):
     img = cv2.imread(filename)
     img = cv2.resize(img, (224,224))[...,::-1]
     img = np.expand_dims(img, axis=0).astype(np.float32)
@@ -53,7 +49,6 @@ def predict(filename):
     return render_template('predict.html',
                            filename=filename,
                            predictions=result)
-
 
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
